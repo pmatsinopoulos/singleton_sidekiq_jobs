027331085164e90685dc93c22666ad31cce9015b
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-04-14 16:54:19 +0300

    Fix the checkin of the job inside the enqueued jobs

--- app/models/concerns/enqueueable_job.rb
+++ app/models/concerns/enqueueable_job.rb
@@ -7,14 +7,28 @@ module EnqueueableJob
     def process_enqueued_job(queueable:)
       enqueued_job = nil
 
-      enqueued_job = EnqueuedJob.find_by(
-        provider_job_id: [ provider_job_id, job_id ] # it seems that in +test+ env +provider_job_id+ is not present
-      )
+      ActiveRecord::Base.transaction do
+        AdvisoryLock.lock(
+          lock_namespace: queueable.queueable_lock_namespace,
+          lock_key: queueable.queueable_lock_key(job_class_name: self.class.name),
+          lock_comment: queueable.queueable_lock_comment(job_class_name: self.class.name)
+        )
+
+        enqueued_job = EnqueuedJob.where(
+          provider_job_id: [ provider_job_id, job_id ] # it seems that in +test+ env +provider_job_id+ is not present
+        ).first
+      end
 
       if enqueued_job.present?
-        yield
+        yield instrumentation_payload
 
-        enqueued_job.destroy! rescue nil
+        Rails.logger.debug("--------------->******************* About to delete EnqueuedJob for provider_job_id: #{provider_job_id}, job_id: #{job_id}")
+
+        begin
+          enqueued_job.destroy!
+        rescue
+          nil
+        end
       end
     end
   end
